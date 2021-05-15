f5414a924e3082571a85dd2225671f08e190103d
nikhil <nikhilatoliya12@gmail.com>
2021-05-15 15:37:28 +0530

    contact success

--- assets/js/success.js
+++ assets/js/success.js
@@ -1,9 +1,8 @@
 function success(){
 	
-    alert("Thank you for submitting");
-       document.getElementById('name').value='';
-       document.getElementById('phone').value='';
-    	document.getElementById('email').value='';
+     //     document.getElementById('name').value='';
+     //   document.getElementById('phone').value='';
+    	// document.getElementById('email').value='';
         
-        document.getElementById('message').value='';
+     //    document.getElementById('message').value='';
 }
