20a8ea4018142656a6593da04b2193177dc1df77
nikhil <nikhilatoliya12@gmail.com>
2021-05-15 13:29:51 +0530

    contact added

--- assets/js/success.js
+++ assets/js/success.js
@@ -1,8 +1,8 @@
 function success(){
 	
     alert("Thank you for submitting");
-     //   document.getElementById('name').value='';
-    	// document.getElementById('mail').value='';
-     //    document.getElementById('phone').value='';
-     //    document.getElementById('message').value='';
+       document.getElementById('name').value='';
+    	document.getElementById('mail').value='';
+        document.getElementById('phone').value='';
+        document.getElementById('message').value='';
 }
