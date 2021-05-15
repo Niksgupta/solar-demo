e9de17235bfc1f9e9c50efee96594377f71ba000
nikhil <nikhilatoliya12@gmail.com>
2021-05-15 13:25:56 +0530

    added contct

---
+++ assets/js/success.js
@@ -0,0 +1,8 @@
+function success(){
+	
+    alert("Thank you for submitting");
+     //   document.getElementById('name').value='';
+    	// document.getElementById('mail').value='';
+     //    document.getElementById('phone').value='';
+     //    document.getElementById('message').value='';
+}
