2539df39dbf217420947fc259bff2e43a810992d
nikhil <nikhilatoliya12@gmail.com>
2021-05-15 13:36:31 +0530

    contact success

--- assets/js/success.js
+++ assets/js/success.js
@@ -2,7 +2,8 @@ function success(){
 	
     alert("Thank you for submitting");
        document.getElementById('name').value='';
-    	document.getElementById('mail').value='';
-        document.getElementById('phone').value='';
+       document.getElementById('phone').value='';
+    	document.getElementById('email').value='';
+        
         document.getElementById('message').value='';
 }
